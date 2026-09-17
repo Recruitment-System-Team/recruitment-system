1cb66b7b043d62cc7f03f3f50d4b60832750ac5f
shahmeemaryam <shahmeemunavver@gmail.com>
2026-09-17 22:28:56 +0530

    Allow GitHub Pages frontend in CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -17,6 +17,7 @@ return [
         'http://localhost:5173',
         'http://localhost:5175',
         'http://localhost:5176',
+        'https://recruitment-system-team.github.io',
     ],
 
     'allowed_origins_patterns' => [],
